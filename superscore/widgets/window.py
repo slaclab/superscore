1c17a4a62b8b35a23a5b2704dfb37490d32cfeb8
Yekta Yazar <yektay1@gmail.com>
2025-05-27 16:41:46 -0700

    added configure page to window.py

--- superscore/widgets/window.py
+++ superscore/widgets/window.py
@@ -49,11 +49,12 @@ class Window(QtWidgets.QMainWindow, metaclass=QtSingleton):
         self.snapshot_details_page = self.init_snapshot_details_page()
         self.pages.add(self.snapshot_details_page)
         self.pv_browser_page = self.init_pv_browser_page()
-
+        self.configure_page = self.init_configure_page()
         self.main_content_stack = QtWidgets.QStackedLayout()
         self.main_content_stack.addWidget(self.view_snapshot_page)
         self.main_content_stack.addWidget(self.snapshot_details_page)
         self.main_content_stack.addWidget(self.pv_browser_page)
+        self.main_content_stack.addWidget(self.configure_page)
         self.main_content_stack.setCurrentWidget(self.view_snapshot_page)
         self.main_content_container = QtWidgets.QWidget()
         self.main_content_container.setContentsMargins(0, 0, 0, 0)
@@ -73,6 +74,7 @@ class Window(QtWidgets.QMainWindow, metaclass=QtSingleton):
         navigation_panel.sigViewSnapshots.connect(self.open_view_snapshot_page)
         navigation_panel.sigBrowsePVs.connect(self.open_pv_browser_page)
         navigation_panel.sigSave.connect(self.take_snapshot)
+        navigation_panel.sigConfigureTags.connect(self.open_configure_page)
         navigation_panel.set_nav_button_selected(navigation_panel.view_snapshots_button)
         navigation_panel.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Preferred)
         return navigation_panel
@@ -83,12 +85,6 @@ class Window(QtWidgets.QMainWindow, metaclass=QtSingleton):
         view_snapshot_layout = QtWidgets.QVBoxLayout()
         view_snapshot_layout.setContentsMargins(0, 11, 0, 0)
         view_snapshot_page.setLayout(view_snapshot_layout)
-
-        self.navigation_panel = NavigationPanel()
-        self.navigation_panel.sigViewSnapshots.connect(self.open_snapshot_table)
-        self.navigation_panel.sigBrowsePVs.connect(self.open_pv_browser_page)
-        self.navigation_panel.sigAdmin.connect(self.open_admin_page)
-        self.navigation_panel.sigConfigureTags.connect(self.open_tag_groups)
 
         self.snapshot_table = QtWidgets.QTableView()
         self.snapshot_table.setModel(SnapshotTableModel(self.client))
@@ -148,6 +144,19 @@ class Window(QtWidgets.QMainWindow, metaclass=QtSingleton):
         pv_browser_layout.addWidget(self.pv_browser_table)
         return pv_browser_page
 
+    def init_configure_page(self) -> QtWidgets.QWidget:
+        """Initialize the configure page with the tag groups window."""
+        configure_page = QtWidgets.QWidget()
+        configure_layout = QtWidgets.QVBoxLayout()
+        configure_layout.setContentsMargins(0, 11, 0, 0)
+        configure_page.setLayout(configure_layout)
+
+        self.tag_groups_window = TagGroupsWindow(self.client)
+        self.tag_groups_window.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
+        configure_layout.addWidget(self.tag_groups_window)
+
+        return configure_page
+
     @QtCore.Slot()
     def open_pv_browser_page(self) -> None:
         """Open the PV Browser Page if it is not already open."""
@@ -161,6 +170,13 @@ class Window(QtWidgets.QMainWindow, metaclass=QtSingleton):
         if self.main_content_stack.currentWidget() != self.view_snapshot_page:
             self.main_content_stack.setCurrentWidget(self.view_snapshot_page)
             self.navigation_panel.set_nav_button_selected(self.navigation_panel.view_snapshots_button)
+
+    @QtCore.Slot()
+    def open_configure_page(self) -> None:
+        """Open the configure page if it is not already open."""
+        if self.main_content_stack.currentWidget() != self.configure_page:
+            self.main_content_stack.setCurrentWidget(self.configure_page)
+            self.navigation_panel.set_nav_button_selected(self.navigation_panel.configure_tags_button)
 
     @QtCore.Slot(QtCore.QModelIndex)
     def open_snapshot_index(self, index: QtCore.Qt.QModelIndex) -> None:
