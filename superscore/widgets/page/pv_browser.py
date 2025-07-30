c31dee9f9c1fd1f71d73e89f3acb00e33408b147
Devan Agrawal <devagr@slac.stanford.edu>
2025-07-30 15:26:52 -0700

    ENH: implement add / delete PV using MongoBackend
    Closes: SWAPPS-328

--- superscore/widgets/page/pv_browser.py
+++ superscore/widgets/page/pv_browser.py
@@ -13,7 +13,8 @@ from superscore.widgets.tag import TagDelegate, TagsWidget
 
 class PVBrowserPage(Page):
 
-    open_details_signal = QtCore.Signal(QtCore.QModelIndex, QtWidgets.QAbstractItemView)
+    sigOpenPVDetails = QtCore.Signal(QtCore.QModelIndex, QtWidgets.QAbstractItemView)
+    sigAddPV = QtCore.Signal()
 
     def __init__(self, client: Client, parent: QtWidgets.QWidget = None):
         super().__init__(parent)
@@ -37,9 +38,10 @@ class PVBrowserPage(Page):
 
         self.add_pv_button = QtWidgets.QPushButton()
         self.add_pv_button.setIcon(qta.icon("ph.plus"))
-        self.add_pv_button.setIconSize(QtCore.QSize(24, 24))
+        self.add_pv_button.setIconSize(QtCore.QSize(16, 16))
         self.add_pv_button.setText("Add PV")
         self.add_pv_button.setObjectName("add-pv-btn")
+        self.add_pv_button.clicked.connect(self.sigAddPV)
 
         spacer = QtWidgets.QSpacerItem(1, 1, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
 
@@ -48,11 +50,6 @@ class PVBrowserPage(Page):
         search_bar_lyt.addSpacerItem(spacer)
         search_bar_lyt.addWidget(self.add_pv_button)
         pv_browser_layout.addLayout(search_bar_lyt)
-
-        permission_manager = PermissionManager.get_instance()
-        if not permission_manager.is_admin():
-            self.add_pv_button.hide()
-        permission_manager.admin_status_changed.connect(self.add_pv_button.setVisible)
 
         filter_tags = TagsWidget(tag_groups=self.client.backend.get_tags(), enabled=True)
         pv_browser_layout.addWidget(filter_tags)
@@ -69,7 +66,7 @@ class PVBrowserPage(Page):
         )
         header_view = self.pv_browser_table.horizontalHeader()
         header_view.setSectionResizeMode(header_view.ResizeMode.Fixed)
-        header_view.setStretchLastSection(True)
+        header_view.setSectionResizeMode(PV_BROWSER_HEADER.TAGS.value, header_view.ResizeMode.Stretch)
         header_view.sectionResized.connect(self.pv_browser_table.resizeRowsToContents)
         pv_browser_layout.addWidget(self.pv_browser_table)
         self.pv_browser_table.resizeColumnsToContents()
@@ -77,6 +74,20 @@ class PVBrowserPage(Page):
         self.search_bar.textEdited.connect(self.search_bar_middle_man)
         filter_tags.tagSetChanged.connect(self.pv_browser_filter.set_tag_set)
         self.pv_browser_table.doubleClicked.connect(self.open_details_middle_man)
+
+        permission_manager = PermissionManager.get_instance()
+        if not permission_manager.is_admin():
+            self.add_pv_button.hide()
+            self.pv_browser_table.setColumnHidden(PV_BROWSER_HEADER.DELETE.value, True)
+        permission_manager.admin_status_changed.connect(self.add_pv_button.setVisible)
+        permission_manager.admin_status_changed.connect(
+            lambda is_admin: self.pv_browser_table.setColumnHidden(
+                PV_BROWSER_HEADER.DELETE.value,
+                not is_admin,
+            )
+        )
+
+        self.pv_browser_table.clicked.connect(self.maybe_delete_row)
 
         self.setStyleSheet(
             """
@@ -95,6 +106,11 @@ class PVBrowserPage(Page):
             """
         )
 
+    @QtCore.Slot(QtCore.QModelIndex)
+    def maybe_delete_row(self, index):
+        if index.column() == PV_BROWSER_HEADER.DELETE.value:
+            self.pv_browser_table.model().sourceModel().removeRow(index.row())
+
     @QtCore.Slot()
     def search_bar_middle_man(self):
         search_text = self.search_bar.text()
@@ -104,4 +120,4 @@ class PVBrowserPage(Page):
     def open_details_middle_man(self, index: QtCore.QModelIndex):
         if not isinstance(index, QtCore.QModelIndex) or not index.isValid():
             return
-        self.open_details_signal.emit(index, self.pv_browser_table)
+        self.sigOpenPVDetails.emit(index, self.pv_browser_table)
